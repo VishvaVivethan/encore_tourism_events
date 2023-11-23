a38772c8ce30d317a80a99b1a521ca07ef77f703
venkatesh ponraj <vengatp14@gmail.com>
2023-11-23 18:13:22 +0530

    init

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  autoplay: 1000,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+  autoplay: {
+    delay: 1000,
+  },
+});
+
+
+/*
+inspiration
+https://cz.pinterest.com/pin/830703093792161391/
+*/
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5
+  },
+  keyboard: {
+    enabled: true
+  },
+  autoplay: {
+    delay: 1000,
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  spaceBetween: 30,
+  loop: false,
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+swiper.slideTo(1, false, false);
+
+
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+
+ 
